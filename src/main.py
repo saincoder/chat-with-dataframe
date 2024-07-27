19d810a19a01367d137eff1f9f73e350ce19c5d6
shahid <kancain00@gmail.com>
2024-07-27 09:41:46 +0500

    create function for file handling also create session state for chat history

--- src/main.py
+++ src/main.py
@@ -5,3 +5,26 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_ollama import ChatOllama
 
 
+# streamlit web app configuration
+
+st.set_page_config(
+    page_icon="💭",
+    page_title="DF chatbot",
+    layout='centered'
+)
+
+# function for read the two different file
+def read_data(file):
+    if file.name.endwith('.csv'):
+        return pd.read_csv(file)
+    
+    else:
+        pd.read_excel(file)
+
+# title
+st.title('🤖 DataFrame Chatbot -Ollama')
+
+
+# Initialize chat history in streamlit session
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
